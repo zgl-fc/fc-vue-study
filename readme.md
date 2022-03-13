a05849a88e1e8ecc29d733d8aabf6ad1063e3139
张飞车 <2088682981@qq.com>
2022-03-13 23:13:06 +0800

    feat(render): complete custom render,wo can pass own render

--- readme.md
+++ readme.md
@@ -6,3 +6,7 @@ emit 原理 其实就是将父组件的函数和其他的props一样传过去，
 可插槽的组件 children 是对象，将对象转换成对应 接受props返回Vnode的函数
 在可插槽组件中调用对应的函数，传入自己的数据,所以插槽作用域是子组件的
 
+# provide inject的原理
+就是每个实例上维护一个 provides 对象 和 一个parent指向父实例
+每个provides对象与父实例构成原型链
+我们读inject的时候是读的 parent.provides上的属性,因为是原型链所以父上面找不到，继续往上找，没有的话就是provide提供的默认值
--- src/runtime-core/index.ts
+++ src/runtime-core/index.ts
@@ -1,6 +1,6 @@
-export { createApp } from './createApp'
 export { h } from './h'
 export { renderSlots } from './renderSlots'
 export { createTextVNode } from './vnode'
 export { getCurrentInstance } from './component'
 export { provide, inject } from './apiInject'
+export { createRenderer } from './renderer'
